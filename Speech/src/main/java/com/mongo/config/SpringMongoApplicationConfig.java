6c0ddaa46d9f5d6aa3ca5db88ab43117452c67ee
Levent <ylaksun@yahoo.com>
2017-12-07 09:23:59 +0000

    mongo db

--- Speech/src/main/java/com/mongo/config/SpringMongoApplicationConfig.java
+++ Speech/src/main/java/com/mongo/config/SpringMongoApplicationConfig.java
@@ -1,5 +1,35 @@
 package com.mongo.config;
 
-public class SpringMongoApplicationConfig {
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+
+@Configuration
+public class SpringMongoApplicationConfig extends AbstractMongoConfiguration {
+	
+	@Override
+    @Bean
+    public Mongo mongo() throws Exception {
+        return new MongoClient(Arrays.asList(new ServerAddress("mongo-db",
+                27017)));
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "local";
+    }
+
+    @Override
+    protected String getMappingBasePackage() {
+        return "com.mongo.model";
+    }
+
+	
 
 }
