109354eb050464e26019f1f33330a08d5ffb185d
Levent <ylaksun@yahoo.com>
2018-04-20 06:47:33 +0100

    first-boot

---
+++ first-web/src/main/java/com/yla/springboot/firstweb/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.yla.springboot.firstweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+
+		@RequestMapping("/login")
+		//@ResponseBody
+		public String LoginMessage() {
+			//return "Hello World Modifier";
+			//returns to login.jsp
+			return "login";
+		}
+}
