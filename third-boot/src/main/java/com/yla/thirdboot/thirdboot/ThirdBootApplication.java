6b8ef2b39b9659f5fd2e699e8a0550e2853d0159
Levent <ylaksun@yahoo.com>
2018-04-25 13:47:48 +0100

    third-boot

---
+++ third-boot/src/main/java/com/yla/thirdboot/thirdboot/ThirdBootApplication.java
@@ -0,0 +1,12 @@
+package com.yla.thirdboot.thirdboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdBootApplication.class, args);
+	}
+}
---
+++ third-boot/src/test/java/com/yla/thirdboot/thirdboot/ThirdBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yla.thirdboot.thirdboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThirdBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
